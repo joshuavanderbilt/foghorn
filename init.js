6ed2aa6a2e388d83ac84b6215b627d17abd3c31b
Joshua <you@example.com>
2023-08-27 23:22:55 -0500

    The panel is now part of init.js instead of index.html and its position can be reset

--- init.js
+++ init.js
@@ -1,5 +1,29 @@
-// Make windows draggable, including panel.
-  $( function() {
-    $( ".window" ).draggable();
-  } );
 
+
+function openpanel() {
+	var desktop = document.getElementById("desktop");
+	desktop.innerHTML += `
+	<div id="panel" class="window" style="position:absolute;top:0px;left:0px;width:640px;height:23px;">
+                        <button onclick="apps()">Applications</button>
+                        <button onclick="launcher()">Launcher</button>
+                        <a href=".."><button>Logoff</button></a>
+			<button style="position:absolute;right:4px;" onclick="resetpanel()">Reposition</button>
+                        </div>
+	`;
+	// Make panel draggable
+	 $( function() {
+    		$( ".window" ).draggable();
+         } );
+}
+
+function closepanel() {
+	const element = document.getElementById("panel");
+	if (element !== null) {
+		element.remove();
+	}
+}
+
+function resetpanel() {
+	closepanel();
+	openpanel();
+}
