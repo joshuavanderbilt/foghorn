cce7a91b51982dfc9b7dadf5cb511733f344e8ca
Joshua Vanderbilt <joshua@localhost.localdomain>
2023-08-29 07:47:50 -0500

    Add more functionality to the calculator

--- calc.js
+++ calc.js
@@ -8,9 +8,9 @@
 function calc() {
 	calcclose();
 	newwindow("calc","Calculator",`
-	<input type="text" id="calctext" style="width:320">
+	<input type="text" id="calctext" style="width:300">
 	<br>
-	<button onclick="calcclear()">C</button><button onclick="calceval()">=</button><button onclick="cins('');">---</button><button onclick="cins('/');">/</button>
+	<button onclick="calcclear()">C</button><button onclick="calceval()">=</button><button onclick="cins('00');">00</button><button onclick="cins('/');">/</button>
 	<br>
 	<button onclick="cins('7');">7</button><button onclick="cins('8');">8</button><button onclick="cins('9');">9</button><button onclick="cins('*');">*</button>
 	<br>
@@ -18,7 +18,7 @@ function calc() {
 	<br>
 	<button onclick="cins('1');">1</button><button onclick="cins('2');">2</button><button onclick="cins('3');">3</button><button onclick="cins('+');">+</button>
 	<br>
-	<button onclick="cins('');">---</button><button onclick="cins('0');">0</button><button onclick="cins('');">---</button><button onclick="cins('');">---</button>
+	<button onclick="cins('(');">(</button><button onclick="cins('0');">0</button><button onclick="cins(')');">)</button><button onclick="cins('.');">.</button>
 	`);
 }
 
