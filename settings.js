ae3e9ff74e01c8fc9a307fb9c77791c36604b3bc
Joshua Vanderbilt <joshua@localhost.localdomain>
2023-08-29 09:02:39 -0500

    Add the ability to store settings in the form of cookies

--- settings.js
+++ settings.js
@@ -4,16 +4,42 @@ function settings() {
 	<button onclick="changebgcolor(document.getElementById('bgtext').value);">Change Background Color (HEX)</button>
 	<button onclick="changebgimage(document.getElementById('bgtext').value);">Change Background Image (URL)</button>
 	<button onclick="defaultbg();">Use Default Background</button>
-	<br>
-	<p>Warning! Settings cannot currently be saved.</p>
 	`);
 }
 function changebgcolor(color) {
 	document.getElementById("desktop").style = ("background-color:" + color);
+	document.cookie = "bgcolor=" + color
+	document.cookie = "bgimage=undefined"
 }
 function changebgimage(image) {
 	document.getElementById("desktop").style = ("background-image:url('" + image + "')");
+	document.cookie = "bgimage=" + image
+	document.cookie = "bgcolor=undefined"
 }
 function defaultbg() {
-	document.getElementById("desktop").style = ("background-image:url('bg.png');");
+	changebgcolor("008080");
+	changebgimage("bg.png");
+}
+
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+function restoresettings() {
+	if (getCookie("bgcolor") !== "undefined" ) {
+		changebgcolor(getCookie("bgcolor"));
+	}
+	if (getCookie("bgimage") !== "undefined") {
+		changebgimage(getCookie("bgimage"));
+	}
+	if (getCookie("bgimage") == "undefined" && getCookie("bgcolor") == "undefined") {
+		changebgimage("bg.png");
+	}
+}
+
+function factoryreset() {
+	document.cookie = "bgimage=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+	document.cookie = "bgcolor=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
 }
