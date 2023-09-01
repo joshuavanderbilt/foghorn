17fe0b0e0ac276ee968afcaaff552e6c8f3cdc0d
Joshua <joshua@joshua>
2023-09-01 17:58:25 -0500

    Remove iframe button from launcher

--- launcher.js
+++ launcher.js
@@ -24,6 +24,5 @@ function launcher() {
 	newwindow("launcher","Launcher",`
 	<input type='text' id='wintext' style='width:640px'>
 	<button onclick="openwindow('Window',document.getElementById('wintext').value);launcherclose();">Make Window</button>
-	<button onclick="iwindow('iFrame',document.getElementById('wintext').value,640,480);launcherclose();">Make iFrame</button>
 	<button onclick="run();launcherclose();">Execute JS</button>`);
 }
