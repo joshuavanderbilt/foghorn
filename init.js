6e637463b92a07076f427dfbea58f0586da42386
Joshua <joshua@joshua>
2023-09-01 11:25:53 -0500

    Add clock to panel

--- init.js
+++ init.js
@@ -8,6 +8,7 @@ function openpanel() {
                         <button onclick="launcher()">Launcher</button>
                         <a href=".."><button>Logoff</button></a>
 			<button style="position:absolute;right:4px;" onclick="resetpanel()">Reposition</button>
+			<span class="status-bar-field" style="position:absolute;right:90px;top:4px;width:54px;height:16px"><center><span style="position:relative;top:2px" id="clock">00:00:00</span></center></span>
                         </div>
 	`;
 	// Make panel draggable
@@ -31,6 +32,18 @@ function resetpanel() {
 	closepanel();
 	openpanel();
 }
+
+// Clock code
+function time(){
+    var d = new Date();
+    var s = d.getSeconds();
+    var m = d.getMinutes();
+    var h = d.getHours();
+    document.getElementById('clock').innerHTML = (h + ":" + m + ":" + s);
+}
+
+setInterval(time,1000);
+// End of Clock
 
 // Set up the desktop.
 window.onload = () => {
